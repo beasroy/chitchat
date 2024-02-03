3b1c494fc6cde95f6436940bd7de4f1908f5e9f6
beasroy <roybipasha676@gmail.com>
2024-02-04 00:58:38 +0530

    backend done

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.js";
+import messageRoutes from "./routes/message.js";
+import userRoutes from "./routes/user.js";
+
+import connectToDb from "./db/connectToDB.js"
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users",userRoutes);
+
+
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
+app.listen(PORT, () => {
+	connectToDb();
+	console.log(`Server Running on port ${PORT}`);
+});
